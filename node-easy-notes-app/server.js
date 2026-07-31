e16c73630c787cb174602910eaf7a674edca768e
pallapualanya <pallapualanya@gmail.com>
2026-07-31 22:32:31 +0530

    fix server

--- node-easy-notes-app/server.js
+++ node-easy-notes-app/server.js
@@ -3,25 +3,28 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
+const dbUrl = process.env.MONGODB_URI || dbConfig.url;
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
+    process.exit(1);
 });
 
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "Welcome to EasyNotes application."});
 });
 
 require('./app/routes/note.routes.js')(app);
